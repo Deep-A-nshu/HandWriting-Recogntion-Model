21a6d8c931cfc68f9b03cc7b391fb5f0adab0807
Deep-A-nshu <92726205+Deep-A-nshu@users.noreply.github.com>
2023-06-14 04:20:46 +0530

    Update Preprocessing.py

--- src/Preprocessing.py
+++ src/Preprocessing.py
@@ -54,37 +54,14 @@ def preprocessor(img, imgSize, enhance=False, dataAugmentation=False):
 
 
 def wer(r, h):
-    """
-    Calculation of WER with Levenshtein distance.
-
-    Works only for iterables up to 254 elements (uint8).
-    O(nm) time ans space complexity.
-
-    Parameters
-    ----------
-    r : list
-    h : list
-
-    Returns
-    -------
-    int
-
-    Examples
-    --------
-    >>> wer("who is there".split(), "is there".split())
-    1
-    >>> wer("who is there".split(), "".split())
-    3
-    >>> wer("".split(), "who is there".split())
-    3
-    """
+    # This is the Dynamic Programming problem of Edit distance.
     # initialisation
     d = np.zeros((len(r)+1)*(len(h)+1), dtype=np.uint8)
     d = d.reshape((len(r)+1, len(h)+1))
     for i in range(len(r)+1):
         for j in range(len(h)+1):
             if i == 0:
-                d[0][j] = j
+                d[0][j] = jv
             elif j == 0:
                 d[i][0] = i
 
